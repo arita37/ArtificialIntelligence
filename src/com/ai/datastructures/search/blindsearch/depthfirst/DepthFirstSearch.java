f0aec9cb9e7934d39654fd12a6e9bb115b483f19
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-14 04:24:51 -0500

    Depth  first Search Implementation

---
+++ src/com/ai/datastructures/search/blindsearch/depthfirst/DepthFirstSearch.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.ai.datastructures.search.blindsearch.depthfirst;
+
+import com.ai.datastructures.trees.TreeNode;
+
+/**
+ * @author Dany
+ *
+ */
+public class DepthFirstSearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Integer doDFS()
+	{
+		TreeNode<Integer, Integer> n=new TreeNode<Integer, Integer>();
+		
+		return 0;
+	}
+
+}
